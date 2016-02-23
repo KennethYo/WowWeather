8177cb8d706e4df0549ffe099ac234651fbac4f3
Kenneth <youzicong@gmail.com>
2016-02-23 14:28:04 +0800

    加入 bugly

---
+++ app/src/main/java/cn/kenneth/wowweather/Constants.java
@@ -0,0 +1,8 @@
+package cn.kenneth.wowweather;
+
+/**
+ * Created by kenneth on 16/2/23.
+ */
+public class Constants {
+    public final static String BUGLY_APP_ID = "900020176";
+}
--- app/src/main/java/cn/kenneth/wowweather/MyApplication.java
+++ app/src/main/java/cn/kenneth/wowweather/MyApplication.java
@@ -2,6 +2,8 @@ package cn.kenneth.wowweather;
 
 import android.app.Application;
 
+import com.tencent.bugly.crashreport.CrashReport;
+
 /**
  * Created by Kenneth on 2016/2/22.
  */
@@ -10,5 +12,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashReport.initCrashReport(getApplicationContext(), Constants.BUGLY_APP_ID, false);
     }
 }
